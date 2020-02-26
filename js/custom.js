5737efbc0e4a66f6e1d02e339daac4ae4ad9dd7e
Yousuf-Gazi <55175452+Yousuf-Gazi@users.noreply.github.com>
2020-02-26 23:23:35 +0600

    rakin portfolio

---
+++ js/custom.js
@@ -0,0 +1,111 @@
+// slick slider
+$('.my_slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+// slick slider
+
+// venobox 
+$('.venobox').venobox(); 
+// venobox 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//     $('.project_slick').slick({
+//         dots: true,
+//         infinite: true,
+//         prevArrow: '.slidPrv',
+//         nextArrow: '.slidNext',
+//         speed: 300,
+//         slidesToShow: 4,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//         responsive: [
+//           {
+//             breakpoint: 1024,
+//             settings: {
+//               slidesToShow: 3,
+//               slidesToScroll: 3,
+//               infinite: true,
+//               dots: true
+//             }
+//           },
+//           {
+//             breakpoint: 600,
+//             settings: {
+//               slidesToShow: 2,
+//               slidesToScroll: 2
+//             }
+//           },
+//           {
+//             breakpoint: 480,
+//             settings: {
+//               slidesToShow: 1,
+//               slidesToScroll: 1
+//             }
+//           }
+//           // You can unslick at a given breakpoint now by adding:
+//           // settings: "unslick"
+//           // instead of a settings object
+//         ]
+//     });
+// });
